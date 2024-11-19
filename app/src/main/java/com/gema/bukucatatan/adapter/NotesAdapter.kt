5f7011de25f090db9dfc19d387cf7b6c83201cc6
gema <gemasyaputra04@gmail.com>
2024-11-19 07:58:45 +0700

    update

--- app/src/main/java/com/gema/bukucatatan/adapter/NotesAdapter.kt
+++ app/src/main/java/com/gema/bukucatatan/adapter/NotesAdapter.kt
@@ -1,6 +1,7 @@
 package com.gema.bukucatatan.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -8,12 +9,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.gema.bukucatatan.Note
 import com.gema.bukucatatan.R
 import android.view.LayoutInflater
+import android.widget.ImageView
+import com.gema.bukucatatan.UpdateNoteActivity
 
 class NotesAdapter (private var notes: List<Note>, context: Context) :
 RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
         val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
+        val updateButton: ImageView = itemView.findViewById(R.id.updateButton)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
@@ -27,10 +31,19 @@ RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
         val note = notes[position]
         holder.titleTextView.text = note.title
         holder.contentTextView.text = note.content
+
+        holder.updateButton.setOnClickListener {
+            val intent = Intent(holder.itemView.context, UpdateNoteActivity::class.java).apply {
+                putExtra("note_id", note.id)
+            }
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
     fun refreshData(newNotes: List<Note>) {
         notes = newNotes
         notifyDataSetChanged()
     }
+
+
 }
