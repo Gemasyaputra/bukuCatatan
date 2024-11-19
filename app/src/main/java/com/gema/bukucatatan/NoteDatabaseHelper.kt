edc61d39e8a7f36cc45265b8fc30f978a874da5d
gema <gemasyaputra04@gmail.com>
2024-11-19 09:05:29 +0700

    update 2

--- app/src/main/java/com/gema/bukucatatan/NoteDatabaseHelper.kt
+++ app/src/main/java/com/gema/bukucatatan/NoteDatabaseHelper.kt
@@ -87,6 +87,13 @@ class NoteDatabaseHelper(context: Context):SQLiteOpenHelper(context, DATABASE_NA
         db.close()
         return Note(id, title, content)
     }
+    fun deleteNote(noteId: Int) {
+        val db = writableDatabase
+        val whereClause = "$COLUMN_ID = ?"
+        val whereArgs = arrayOf(noteId.toString())
+        db.delete(TABLE_NAME, whereClause, whereArgs)
+        db.close()
+    }
 
 
 }
--- app/src/main/java/com/gema/bukucatatan/adapter/NotesAdapter.kt
+++ app/src/main/java/com/gema/bukucatatan/adapter/NotesAdapter.kt
@@ -10,14 +10,19 @@ import com.gema.bukucatatan.Note
 import com.gema.bukucatatan.R
 import android.view.LayoutInflater
 import android.widget.ImageView
+import android.widget.Toast
+import com.gema.bukucatatan.NoteDatabaseHelper
 import com.gema.bukucatatan.UpdateNoteActivity
 
 class NotesAdapter (private var notes: List<Note>, context: Context) :
 RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+    private val db: NoteDatabaseHelper = NoteDatabaseHelper(context)
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
         val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
         val updateButton: ImageView = itemView.findViewById(R.id.updateButton)
+        val deleteButton: ImageView = itemView.findViewById(R.id.deleteButton)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
@@ -37,6 +42,12 @@ RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
                 putExtra("note_id", note.id)
             }
             holder.itemView.context.startActivity(intent)
+        }
+
+        holder.deleteButton.setOnClickListener {
+            db.deleteNote(note.id) // Hapus catatan berdasarkan ID
+            refreshData(db.getAllNotes()) // Perbarui daftar catatan
+            Toast.makeText(holder.itemView.context, "Note Deleted", Toast.LENGTH_SHORT).show()
         }
     }
 
